8f202ffef50924fee865429137c50582b1baaf87
Noah Connolly <noah.d.connolly@gmail.com>
2019-03-22 14:16:39 -0400

    Demo version (While Loop)

--- knn-algorithm.py
+++ knn-algorithm.py
@@ -11,10 +11,11 @@ def loadDataset(filename, trainingSet=[], testSet=[]):
         for x in range(len(dataset) - 1):
             for y in range(4):
                 dataset[x][y] = float(dataset[x][y])
-            if x < 40 or x > 59:
+            if x % 5 != 0:
                 trainingSet.append(dataset[x])
             else:
                 testSet.append(dataset[x])
+
 
 def loadDatasetRandom(filename, trainingSet=[], testSet=[]):
     with open(filename, 'rt') as csvfile:
@@ -27,7 +28,6 @@ def loadDatasetRandom(filename, trainingSet=[], testSet=[]):
                 trainingSet.append(dataset[x])
             else:
                 testSet.append(dataset[x])
-
 
 
 def euclideanDistance(instance1, instance2, length):
@@ -66,18 +66,6 @@ def classVote(neighbors):
         return option_2
 
 
-def getResponse(neighbors):
-    classVotes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
-        else:
-            classVotes[response] = 1
-    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
-
-
 def getAccuracy(testSet, predictions):
     correct = 0
     for x in range(len(predictions)):
@@ -85,76 +73,37 @@ def getAccuracy(testSet, predictions):
             correct += 1
     return (correct / float(len(testSet))) * 100.0
 
-def cross_validation():
-    trainingSet = []
-    testSet = []
-    loadDatasetRandom('iris.data', trainingSet, testSet)
-    predictions = []
-    k = 5
-    for i in range(1,5):
-        for x in range(len(testSet)):
-            neighbors = getNeighbors(trainingSet, testSet[x], k)
-            result = classVote(neighbors)
-            predictions.append(result)
-            print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][4]))
-        accuracy = getAccuracy(testSet, predictions)
-        all_accuracies = []
-        all_accuracies.append(accuracy)
-    for i in range(len(all_accuracies)):
-        accuracy_total = 0
-        accuracy_total += all_accuracies[i]
-    kfold_avg = accuracy_total / 5
-    print("5 Fold Cross Validation of Iris Dataset =", kfold_avg,"%")
-
-
 
 def main():
-    # dataSet = input("What dataset would you like to run? \n").lower().strip()
-    dataSet = 'iris'
-    if dataSet == "iris":
-        # prepare data
-        trainingSet = []
-        testSet = []
-        # split = 0.2
-        loadDataset('iris.data', trainingSet, testSet)
-        print
-        'Train set: ' + repr(len(trainingSet))
-        print
-        'Test set: ' + repr(len(testSet))
-        # generate predictions
-        predictions = []
-        k = int(input("How many neighbors would you like to use this time? \n"))
-        # for i in range(1, 10):
-        #     if i % 2 != 0:
-        #         k = i
-        for x in range(len(testSet)):
-            neighbors = getNeighbors(trainingSet, testSet[x], k)
-            result = classVote(neighbors)
-            predictions.append(result)
-            print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][4]))
-        accuracy = getAccuracy(testSet, predictions)
-        print('Accuracy of k = ', k, ' : ' + repr(accuracy) + '%')
+    run = True
+    while run:
+        dataSet = input("What dataset would you like to run? \n").lower().strip()
+        if dataSet == "iris":
+            # prepare data
+            trainingSet = []
+            testSet = []
+            loadDataset('iris.data', trainingSet, testSet)
+            print
+            'Train set: ' + repr(len(trainingSet))
+            print
+            'Test set: ' + repr(len(testSet))
+            # generate predictions
+            predictions = []
+            k = int(input("How many neighbors would you like to use this time? \n"))
+            for x in range(len(testSet)):
+                neighbors = getNeighbors(trainingSet, testSet[x], k)
+                result = classVote(neighbors)
+                predictions.append(result)
+                # Prints predictions and actual type
+                # print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][4]))
+            accuracy = getAccuracy(testSet, predictions)
+            print('Accuracy of k = ', k, ' : ' + repr(accuracy) + '%')
+            keepRunning = input("Would you like the run the software again? (y/n) \n").lower().strip()
+            if keepRunning == 'n':
+                run = False
+        else:
+            print("Not a valid dataset")
 
 
 main()
-#cross_validation()
-# k = 9
-#
-# trainingSet = []
-# testSet = []
-# predictions = []
-#
-# loadDataset('iris.data', trainingSet, testSet)
-# neighbors = getNeighbors(trainingSet, testSet[16], k)
-#
-# print(testSet[16][4])
-# print(neighbors)
-# #print(neighbors[0][4])
-# result = classVote(neighbors)
-# predictions.append(result)
-# print(result)
-# # print('> predicted=' + repr(result) + ', actual=' + repr(testSet[16][4]))
-# accuracy = getAccuracy(testSet, predictions)
-# print('Accuracy of k = ', k, ' : ' + repr(accuracy) + '%')
-
 
